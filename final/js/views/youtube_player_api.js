d22fac373baa014b0f172be5712191ac67dec7e1
Oren Farhi <oren@orizens.com>
2012-12-12 14:47:18 +0200

    youtube js api player is the player now

---
+++ final/js/views/youtube_player_api.js
@@ -0,0 +1,106 @@
+Echoes.Views.YoutubePlayerApi = Backbone.View.extend({
+	el: '#youtube-player-container',
+
+	events: {
+		'click .hide-player': 'hide',
+		'click .show-player': 'show'
+		// 'click .pause': 'pause',
+		// 'click .play': 'playVideo',
+		// 'click .volume-down': 'decreaseVolume',
+		// 'click .volume-up': 'increaseVolume'
+	},
+
+	initialize: function() {
+		this.model.on('change:mediaId', this.play, this);
+		window.onYouTubeIframeAPIReady = _.bind(this.createPlayer, this);
+		$.getScript('http://www.youtube.com/iframe_api?&ghost=Nan');
+	},
+
+	createPlayer: function(){
+		this.player = new YT.Player('player', {
+			height: '270',
+			width: '350',
+			playlist: '',
+			playerVars: { 'autoplay': 1, 'enablejsapi': 1 },
+			events: {
+				'onReady': _.bind(this.onPlayerReady, this),
+				'onStateChange': _.bind(this.onPlayerStateChange, this)
+			}
+		});
+	},
+	
+	onPlayerReady: function(){
+		if (this.queue) {
+			this.play(this.model, this.queue);
+		}
+	},
+
+	onPlayerStateChange: function(ev){
+		// should creat a player model
+		if (ev.data === YT.PlayerState.PAUSED) {
+			this.toggleNowPlaying(false);
+		}
+
+		if (ev.data === YT.PlayerState.PLAYING) {
+			// this.model.fetchCurrentMediaInfo();
+			this.toggleNowPlaying(true);
+		}
+	},
+
+	play: function(model, mediaId) {
+		if (!this.player || !this.player.loadVideoById) {
+			this.show();
+			this.queue = mediaId;
+			return;
+		}
+		this.player.stopVideo();
+		if (this.player.clearVideo) { this.player.clearVideo(); }
+		this.playMedia(mediaId);
+		this.toggleNowPlaying(true);
+		this.show();
+	},
+
+	/**
+	 * plays a single video or a playlist
+	 * @param {json} mediaData - youtube api item result
+	 * @param {json} options - key-value properties of media - type: video/playlist
+	 */
+	playMedia: function(mediaData) {
+		var mediaId = _.isObject(mediaData) ? mediaData.id : mediaData;
+		this.player.loadVideoById(mediaId);
+	},
+
+	pause: function(ev) {
+		ev.preventDefault();
+		this.player.pauseVideo();
+	},
+
+	playVideo: function(ev) {
+		if (ev) { ev.preventDefault(); }
+		this.player.playVideo();
+	},
+
+	decreaseVolume: function(ev) {
+		if (ev) { ev.preventDefault(); }
+		this.player.setVolume(this.player.getVolume() - 5);
+	},
+
+	increaseVolume: function(ev) {
+		if (ev) { ev.preventDefault(); }
+		this.player.setVolume(this.player.getVolume() + 5);
+	},
+
+	toggleNowPlaying: function(show){
+		this.$el.toggleClass('yt-playing', show);
+	},
+
+	show: function(ev) {
+		if (ev) { ev.preventDefault(); }
+		this.$el.addClass('show-youtube-player');
+	},
+
+	hide: function(ev) {
+		ev.preventDefault();
+		this.$el.removeClass('show-youtube-player');
+	}
+});
